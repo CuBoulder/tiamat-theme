f581adb05eeda2839d8d12f7e3783916a3142598
Pat <breepatbree@gmail.com>
2023-01-05 16:40:32 -0700

    Removed extra script

--- js/mainstay.js
+++ js/mainstay.js
@@ -2,16 +2,12 @@ class MainStay extends HTMLElement{
     constructor() {
 		super();
         const id = this.getAttribute('id')
-        const precollege = this.getAttribute('college')
-
-        // Sanitize
-        const college = precollege.replace(/[\W_]+/g, '');
+        const college = this.getAttribute('college')
 		this.build(id, college)
 	}
 
     build(id, college){
-        let tokenScript = document.createElement('script')
-        tokenScript.innerText= `window.admitHubBot = {botToken: "${id}", collegeId: "${college}" };`
+        window.admitHubBot = {botToken: id, collegeId: college};
  
         let chatScript = document.createElement('script')
         chatScript.src="https://webbot.mainstay.com/static/js/webchat.js"
@@ -20,7 +16,6 @@ class MainStay extends HTMLElement{
         stylesheet.type ="text/css"
         stylesheet.href="https://webbot.mainstay.com/static/css/webchat.css"
  
-        this.appendChild(tokenScript)
         this.appendChild(chatScript)
         this.appendChild(stylesheet)
     }
