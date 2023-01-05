0850f1ab827c16ef36c38a2dc69af4831425a341
Pat <breepatbree@gmail.com>
2023-01-05 16:16:27 -0700

    Mainstay script fix, sanitize collegeId

--- js/mainstay.js
+++ js/mainstay.js
@@ -2,19 +2,23 @@ class MainStay extends HTMLElement{
     constructor() {
 		super();
         const id = this.getAttribute('id')
-		this.build(id)
+        const precollege = this.getAttribute('college')
+
+        // Sanitize
+        const college = precollege.replace(/[\W_]+/g, '');
+		this.build(id, college)
 	}
 
-    build(id){
+    build(id, college){
         let tokenScript = document.createElement('script')
-        tokenScript.innerText= `window.admitHubBot = {botToken: "${id}" };`
+        tokenScript.innerText= `window.admitHubBot = {botToken: "${id}", collegeId: "${college}" };`
  
         let chatScript = document.createElement('script')
-        chatScript.src="https://webbot.admithub.com/static/js/webchat.js"
+        chatScript.src="https://webbot.mainstay.com/static/js/webchat.js"
         let stylesheet = document.createElement('link')
         stylesheet.rel = "stylesheet"
         stylesheet.type ="text/css"
-        stylesheet.href="https://webbot.admithub.com/static/css/webchat.css"
+        stylesheet.href="https://webbot.mainstay.com/static/css/webchat.css"
  
         this.appendChild(tokenScript)
         this.appendChild(chatScript)
