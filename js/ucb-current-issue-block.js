c1066a6dc026c3fba042ea6ded62519c7d59430e
Pat <breepatbree@gmail.com>
2023-01-23 16:23:35 -0700

    Error handling for no Issues, fetch error

--- js/ucb-current-issue-block.js
+++ js/ucb-current-issue-block.js
@@ -19,49 +19,72 @@ class CurrentIssueElement extends HTMLElement {
     }
 
     build(data){
-       console.log(data)
-        const title = data.data[0].attributes.title
-        const imgURL = data.included[0].attributes.uri.url
-        const issueURL = data.data[0].attributes.path.alias
+        console.log(data)
+        if(data.data.length == 0){
+            this.handleError({name : "No Issues Retrieved", message : "There are no Issues created"} , 'No Issues Found')
+        } else {
+            const title = data.data[0].attributes.title
+            const imgURL = data.included[0].attributes.uri.url
+            const issueURL = data.data[0].attributes.path.alias
+    
+            const imgLinkEL = document.createElement('a')
+            imgLinkEL.href = issueURL
+    
+            let blockDiv = document.createElement('div')
+            blockDiv.classList='ucb-current-issue-block-content'
+    
+            const imgDiv = document.createElement('div')
+            imgDiv.classList = 'ucb-current-issue-block-img-container'
+    
+    
+            const titleDiv = document.createElement('div')
+            titleDiv.classList = 'ucb-current-issue-block-title-container'
+    
+            const titleEl = document.createElement('h3')
+            titleEl.classList = "ucb-current-issue-block-title"
+            titleEl.innerText = title
+    
+            const imgEL = document.createElement('img')
+            imgEL.classList = 'ucb-current-issue-block-img'
+            imgEL.src = imgURL
+    
+            const linkEl = document.createElement('a')
+            linkEl.href = issueURL
+    
+    
+            linkEl.appendChild(titleEl)
+            titleDiv.appendChild(linkEl)
+    
+            imgLinkEL.appendChild(imgEL)
+            imgDiv.appendChild(imgLinkEL)
+            
+            blockDiv.appendChild(imgDiv)
+            blockDiv.appendChild(titleDiv)
+    
+            this.appendChild(blockDiv)
+        }
+    }
 
-        const imgLinkEL = document.createElement('a')
-        imgLinkEL.href = issueURL
+    handleError(Error, ErrorMsg = 'Error Fetching issues - Check the console'){
+        const container = document.createElement('div');
+        container.className = 'ucb-current-issue-block-content';
+        const span = document.createElement('span')
+        span.classList = "ucb-status-block-link"
+        container.appendChild(span)
 
-        let blockDiv = document.createElement('div')
-        blockDiv.classList='ucb-current-issue-block-content'
+        const icon = document.createElement('i');
+        icon.className = 'fas fa-exclamation-triangle'
+        span.appendChild(icon)
 
-        const imgDiv = document.createElement('div')
-        imgDiv.classList = 'ucb-current-issue-block-img-container'
+        const message = document.createElement('p');
+        message.className = 'ucb-current-issue-block-message'
+        message.innerText = ErrorMsg
+        span.appendChild(message)
 
-
-        const titleDiv = document.createElement('div')
-        titleDiv.classList = 'ucb-current-issue-block-title-container'
-
-        const titleEl = document.createElement('h3')
-        titleEl.classList = "ucb-current-issue-block-title"
-        titleEl.innerText = title
-
-        const imgEL = document.createElement('img')
-        imgEL.classList = 'ucb-current-issue-block-img'
-        imgEL.src = imgURL
-
-        const linkEl = document.createElement('a')
-        linkEl.href = issueURL
-
-
-        linkEl.appendChild(titleEl)
-        titleDiv.appendChild(linkEl)
-
-        imgLinkEL.appendChild(imgEL)
-        imgDiv.appendChild(imgLinkEL)
+        this.appendChild(container)
+       
+            console.error(Error)
         
-        blockDiv.appendChild(imgDiv)
-        blockDiv.appendChild(titleDiv)
-
-        this.appendChild(blockDiv)
-
-
-
     }
 }
 
