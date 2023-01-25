e1542a03c5a61b22575c0f7c6eed612a9285bc01
Pat <breepatbree@gmail.com>
2023-01-25 15:17:40 -0700

    wip: base logic, preliminary styling

--- js/ucb-issue-archive.js
+++ js/ucb-issue-archive.js
@@ -22,6 +22,48 @@ class IssueArchiveElement extends HTMLElement {
             this.handleError({name : "No Issues Retrieved", message : "There are no Issues created"} , 'Create Issues for All Issues to appear here')
         } else {
             console.log(data)
+            const archiveContainer = document.createElement('div')
+            archiveContainer.classList='ucb-issue-archive-container'
+            const issues = data.data
+            const issuesArt = data.included
+
+            for(let i=0;i<issues.length;i++){
+                const issue = issues[i]
+                const issueArt = issuesArt[i]
+
+                const title = issue.attributes.title
+                const imgUrl = issueArt.attributes.uri.url
+                const issueUrl = issue.attributes.path.alias
+
+                const issueContainer = document.createElement('div')
+                issueContainer.classList = 'ucb-issue-archive-card col-sm-2 mx-3'
+
+                const issueImg = document.createElement('img')
+                issueImg.classList = 'ucb-archive-card-img'
+                issueImg.src = imgUrl
+                issueImg.alt = `${title} issue artwork`
+
+                const IssueImgLink = document.createElement('a')
+                IssueImgLink.classList = 'ucb-archive-card-img-link'
+                IssueImgLink.href = issueUrl
+
+                const issueTitle = document.createElement('h3')
+                issueTitle.classList = 'ucb-archive-card-title'
+                issueTitle.innerText = title
+
+                const titleLink = document.createElement('a')
+                titleLink.classList = 'ucb-archive-card-link'
+                titleLink.href = issueUrl
+                titleLink.appendChild(issueTitle)
+
+                IssueImgLink.appendChild(issueImg)
+                issueContainer.appendChild(IssueImgLink)
+                issueContainer.appendChild(titleLink)
+                archiveContainer.appendChild(issueContainer)
+            }
+
+            this.appendChild(archiveContainer)
+
         }
     }
 
