7454c2932c2ac920b9e65714a489e0a0823df83a
Pat <breepatbree@gmail.com>
2023-03-08 12:17:56 -0700

    wip: Link styling, button only appears on email version of newsletter page

--- js/ucb-newsletter.js
+++ js/ucb-newsletter.js
@@ -409,6 +409,34 @@
             }
         }
 
+        // Section Link - make blue (gets overridden by user agent style)
+        var moreLinks = document.getElementsByClassName('article-link')
+        for(link of moreLinks){
+            for(child of link.children)
+            child.style.cssText = `background-color: #0277BD !important;
+            color: #fff !important;
+            display: inline-block !important;
+            padding: 5px 10px !important;
+            font-weight: bold !important;
+            transition: background-color 0.5s ease !important;
+            margin-bottom: 5px !important;
+            text-decoration: none !important;
+            -webkit-border-radius: 3px !important;
+            -moz-border-radius: 3px !important;
+            border-radius: 3px !important;
+            -webkit-background-clip: padding-box !important;
+            -moz-background-clip: padding;
+            background-clip: padding-box;
+            -webkit-box-shadow: 0 1px 2px rgba(0,0,0,0.2);
+            -moz-box-shadow: 0 1px 2px rgba(0,0,0,0.2);
+            box-shadow: 0 1px 2px rgba(0,0,0,0.2);
+            transition: -webkit-box-shadow 0.5s ease,background-color 0.5s ease,color 0.5s ease;
+            transition: -moz-box-shadow 0.5s ease,background-color 0.5s ease,color 0.5s ease;
+            transition: box-shadow 0.5s ease,background-color 0.5s ease,color 0.5s ease;
+            background-color: #0277BD;
+            color: #fff !important;`
+        }
+
         // Stick the styles in the header of the email
         var style = document.getElementsByTagName('style')
         var email = document.getElementById('email-preview')
