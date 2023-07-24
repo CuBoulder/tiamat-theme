d58f14f0afc63395ef8d49f6668b52067970efbf
Pat <breepatbree@gmail.com>
2023-07-24 16:01:02 -0600

    Remove unused API vars from person page template, new api call filters on published, sorts desc

--- js/ucb-person-article-list.js
+++ js/ucb-person-article-list.js
@@ -4,7 +4,7 @@ class PersonArticleList extends HTMLElement {
         // Article Endpoint
         var nodeID = this.getAttribute('nodeId');
         console.log('node id', nodeID)
-        var API = `http://localhost:61658/jsonapi/node/ucb_article?include=field_ucb_article_byline.field_author_person_page&filter[field_ucb_article_byline.field_author_person_page.meta.drupal_internal__target_id]=${nodeID}`
+        var API = `http://localhost:61658/jsonapi/node/ucb_article?include=field_ucb_article_byline.field_author_person_page&filter[field_ucb_article_byline.field_author_person_page.meta.drupal_internal__target_id]=${nodeID}&filter[publish-check][condition][path]=status&filter[publish-check][condition][value]=1&filter[publish-check][condition][memberOf]=published&page[limit]=10&sort[sort-created][path]=created&sort[sort-created][direction]=DESC`
 
         fetch(API)
             .then(this.handleError)
