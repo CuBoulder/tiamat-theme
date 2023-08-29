2b6ea448be1db13fed5f5c6b6962d27aaeec449f
Jacob Korf <korfj921921@gmail.com>
2023-08-29 10:04:49 -0600

    tabs change

--- js/expandable-content.js
+++ js/expandable-content.js
@@ -3,20 +3,23 @@
     var accordionSection = window.location.hash;
     var expandableLocation = document.getElementById(accordionSection.substring(1));
     if (accordionSection.length) {
-        const accordionHeaders = expandableLocation.parentNode.parentNode.parentNode.getElementsByClassName("accordion-button");
+        const accordionHeaders = expandableLocation.parentNode.parentNode.parentNode.getElementsByClassName("expandableHeaders");
         const accordionTexts = expandableLocation.parentNode.parentNode.parentNode.getElementsByClassName("accordion-collapse");
         for (let i = 0; i < accordionHeaders.length; i++) {// header.classList.add("collapsed");
             accordionHeaders[i].setAttribute("aria-expanded", "false")
+            accordionHeaders[i].setAttribute("aria-selected", "false")
             accordionHeaders[i].classList.add("collapsed");
-            accordionTexts[i].classList.remove('show');
+            //accordionTexts[i].classList.remove('show');
           }
        expandableLocation.classList.remove('collapsed');
-       expandableLocation.setAttribute("aria-expanded", "true")
+       expandableLocation.setAttribute("aria-expanded", "true");
+       expandableLocation.setAttribute("aria-selected", "true");
        expandableLocation.parentNode.parentNode.parentNode.classList.add('hello');
        const hello = expandableLocation.parentNode.parentNode.getElementsByClassName("accordion-collapse");
-
+          if(hello.length) {
        hello[0].classList.add("show");
-       alert("yuh");
+
+          }
     }
  
  })();
