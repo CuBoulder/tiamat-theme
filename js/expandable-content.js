8b4664b6b60b8d667ef5cbdbf7702ed6e211be80
Jacob Korf <korfj921921@gmail.com>
2023-08-29 10:15:24 -0600

    clean up code

--- js/expandable-content.js
+++ js/expandable-content.js
@@ -3,13 +3,13 @@
     var accordionSection = window.location.hash;
     var expandableLocation = document.getElementById(accordionSection.substring(1));
     if (accordionSection.length) {
-        const accordionHeaders = expandableLocation.parentNode.parentNode.parentNode.getElementsByClassName("expandableHeaders");
+        const expandableHeaders = expandableLocation.parentNode.parentNode.parentNode.getElementsByClassName("expandableHeaders");
         const accordionTexts = expandableLocation.parentNode.parentNode.parentNode.getElementsByClassName("accordion-collapse");
         for (let i = 0; i < accordionHeaders.length; i++) {
-            accordionHeaders[i].setAttribute("aria-expanded", "false")
-            accordionHeaders[i].setAttribute("aria-selected", "false")
-            accordionHeaders[i].classList.add('collapsed');
-            accordionHeaders[i].classList.remove("active");
+            expandableHeaders[i].setAttribute("aria-expanded", "false")
+            expandableHeaders[i].setAttribute("aria-selected", "false")
+            expandableHeaders[i].classList.add('collapsed');
+            expandableHeaders[i].classList.remove("active");
           }
           for (let i = 0; i < accordionTexts.length; i++) {
             accordionTexts[i].classList.remove('show');
@@ -19,9 +19,9 @@
        expandableLocation.setAttribute("aria-expanded", "true");
        expandableLocation.setAttribute("aria-selected", "true");
        expandableLocation.parentNode.parentNode.parentNode.classList.add('hello');
-       const hello = expandableLocation.parentNode.parentNode.getElementsByClassName("accordion-collapse");
-          if(hello.length) {
-       hello[0].classList.add("show");
+       const accordionDiv = expandableLocation.parentNode.parentNode.getElementsByClassName("accordion-collapse");
+          if(accordionDiv.length) {
+       accordionDiv[0].classList.add("show");
 
           }
     }
