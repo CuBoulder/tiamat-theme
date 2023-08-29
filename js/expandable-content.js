0bd8dcdd2ccf3aa9a57bc1a928ef612a1ff9dcfc
Jacob Korf <korfj921921@gmail.com>
2023-08-29 10:11:21 -0600

    fix expandable tabs

--- js/expandable-content.js
+++ js/expandable-content.js
@@ -5,13 +5,17 @@
     if (accordionSection.length) {
         const accordionHeaders = expandableLocation.parentNode.parentNode.parentNode.getElementsByClassName("expandableHeaders");
         const accordionTexts = expandableLocation.parentNode.parentNode.parentNode.getElementsByClassName("accordion-collapse");
-        for (let i = 0; i < accordionHeaders.length; i++) {// header.classList.add("collapsed");
+        for (let i = 0; i < accordionHeaders.length; i++) {
             accordionHeaders[i].setAttribute("aria-expanded", "false")
             accordionHeaders[i].setAttribute("aria-selected", "false")
-            accordionHeaders[i].classList.add("collapsed");
-            //accordionTexts[i].classList.remove('show');
+            accordionHeaders[i].classList.add('collapsed');
+            accordionHeaders[i].classList.remove("active");
+          }
+          for (let i = 0; i < accordionTexts.length; i++) {
+            accordionTexts[i].classList.remove('show');
           }
        expandableLocation.classList.remove('collapsed');
+       expandableLocation.classList.add('active');
        expandableLocation.setAttribute("aria-expanded", "true");
        expandableLocation.setAttribute("aria-selected", "true");
        expandableLocation.parentNode.parentNode.parentNode.classList.add('hello');
