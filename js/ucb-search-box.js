ee2fa5d4fdf1a6e8d0feb5dafcf61ed4e58dab4e
Timur Tripp <timur@timurtripp.com>
2023-10-27 15:03:14 -0600

    CuBoulder/tiamat-theme#266 adds search modal (3)

--- js/ucb-search-box.js
+++ js/ucb-search-box.js
@@ -1,20 +1,28 @@
 (function (document) {
+
   function selectSearchOption(radio) {
     const searchForm = radio.parentElement.parentElement.parentElement;
     searchForm.querySelector('.ucb-search-input-text input').setAttribute('placeholder', radio.dataset['placeholder']);
     searchForm.setAttribute('action', radio.dataset['action']);
   }
-  const searchBoxOptions = document.getElementsByClassName('ucb-search-box-options');
-  for (let i = 0; i < searchBoxOptions.length; i++) {
-    const radios = searchBoxOptions[i].getElementsByTagName('input');
+
+  const searchBoxes = document.getElementsByClassName('ucb-search-box');
+  for (let i = 0; i < searchBoxes.length; i++) {
+    const searchBoxOptions = searchBoxes[i].querySelector('.ucb-search-box-options');
+    const radios = searchBoxOptions.getElementsByTagName('input');
+    searchBoxes[i].querySelector('form').onsubmit = function (event) {
+      for (let j = 0; j < radios.length; j++)
+        radios[j].removeAttribute('name');
+    };
     for (let j = 0; j < radios.length; j++) {
       if (radios[j].checked)
         selectSearchOption(radios[j]);
-      radios[j].parentElement.addEventListener('change', function (event) {
+      radios[j].addEventListener('change', function (event) {
         const radio = event.target;
         if (radio.checked)
           selectSearchOption(radio);
       });
     }
   }
+
 })(document);
