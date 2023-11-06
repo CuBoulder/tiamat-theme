dfa8f2784223fcff5d9f36ee30065aa80faf3780
Timur Tripp <timur@timurtripp.com>
2023-11-06 16:37:13 -0700

    CuBoulder/tiamat-theme#538 Ensures the `ucb-main-menu-nav` element id isn‘t duplicated if the sticky menu is enabled

--- js/ucb-menu.js
+++ js/ucb-menu.js
@@ -1,22 +1,24 @@
 
 document.addEventListener("DOMContentLoaded", () => {
+  const mainMenuElement = document.querySelector('.ucb-main-menu');
   const mainMenu = new AccessibleMenu.DisclosureMenu({
-    menuElement: document.querySelector("#ucb-main-menu"),
-    controllerElement: document.querySelector("#example-toggle"),
-    containerElement: document.querySelector("#ucb-main-menu-nav"),
+    menuElement: mainMenuElement,
+    controllerElement: document.querySelector("#ucb-mobile-menu-toggle"),
+    containerElement: mainMenuElement,
     openClass: "open",
   });
+
   const secondaryWrapper = document.querySelector(".ucb-secondary-menu-region-container")
   const secondaryMenu = new AccessibleMenu.DisclosureMenu({
     menuElement: document.getElementById("ucb-secondary-menu-region-container").getElementsByClassName("ucb-menu")[0],
-    controllerElement: document.querySelector("#example-toggle"),
+    controllerElement: document.querySelector("#ucb-mobile-menu-toggle"),
     containerElement: secondaryWrapper,
     openClass: "open",
   });
 
   const footerMenu = new AccessibleMenu.DisclosureMenu({
     menuElement: document.getElementById("ucb-mobile-footer-menu").getElementsByClassName("ucb-menu")[0],
-    controllerElement: document.querySelector("#example-toggle"),
+    controllerElement: document.querySelector("#ucb-mobile-menu-toggle"),
     containerElement: secondaryWrapper,
     openClass: "open",
   });
