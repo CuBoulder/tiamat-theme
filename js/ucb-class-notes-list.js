314ac7aae14a8145eb2aecfe600773f783976151
Pat <breepatbree@gmail.com>
2024-01-05 10:57:29 -0700

    wip: setting up custom web component

--- js/ucb-class-notes-list.js
+++ js/ucb-class-notes-list.js
@@ -1,1 +1,31 @@
 console.log("Attached")
+
+class ClassNotesListElement extends HTMLElement {
+
+	constructor() {
+		super();
+		const JSONURL = this.getAttribute('base-uri');
+        console.log(JSONURL)
+	}
+
+	build() {
+	}
+
+	toggleMessageDisplay(element, display, className, innerText) {
+		if(className)
+			element.className = className;
+		if(innerText)
+			element.innerText = innerText;
+		if(display === 'none')
+			element.setAttribute('hidden', '');
+		else element.removeAttribute('hidden');
+	}
+
+	generateForm(){
+	}
+
+	generateDropdown(){
+	}
+}
+
+customElements.define('ucb-class-notes-list', ClassNotesListElement);
