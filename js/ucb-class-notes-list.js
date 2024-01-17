d635886cf16033765bee225286c3afca291abd96
Pat <breepatbree@gmail.com>
2024-01-17 10:16:52 -0700

    wip: year selects and list clears working

--- js/ucb-class-notes-list.js
+++ js/ucb-class-notes-list.js
@@ -3,8 +3,11 @@ class ClassNotesListElement extends HTMLElement {
 		super();
 		const 			
 			chromeElement = this._chromeElement = document.createElement('div'),
-			userFormElement = this._userFormElement = document.createElement('div');
+			userFormElement = this._userFormElement = document.createElement('div'),
+			notesListElement = this._notesListElement = document.createElement('div');
+			this._notesListElement.classList.add('ucb-class-notes-list-container')
 			chromeElement.appendChild(userFormElement);
+			chromeElement.appendChild(notesListElement)
 		this.appendChild(chromeElement);
         const dates = ['--Select date--', 1900, 1901, 1902, 1903, 1904, 1905, 1906, 1907, 1908, 1909, 1910, 1911, 1912, 1913, 1914, 1915, 1916, 1917, 1918, 1919, 1920, 1921, 1922, 1923, 1924, 1925, 1926, 1927, 1928, 1929, 1930, 1931, 1932, 1933, 1934, 1935, 1936, 1937, 1938, 1939, 1940, 1941, 1942, 1943, 1944, 1945, 1946, 1947, 1948, 1949, 1950, 1951, 1952, 1953, 1954, 1955, 1956, 1957, 1958, 1959, 1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034, 2035, 2036, 2037, 2038, 2039, 2040, 2041, 2042, 2043, 2044, 2045, 2046, 2047, 2048, 2049, 2050]
 		const JSONURL = this.getAttribute('base-uri');
@@ -18,7 +21,6 @@ class ClassNotesListElement extends HTMLElement {
 		let yearFilter = '';
 		let publishFilter = ''
 		// If its not a next link, build the JSON API URL
-		if(!next){
 			if (year) {
 				yearFilter = `?filter[field_ucb_class_year]=${year}`
 				publishFilter = '&filter[status]=1'
@@ -26,21 +28,21 @@ class ClassNotesListElement extends HTMLElement {
 				yearFilter = ''
 				publishFilter = '?filter[status]=1'
 			}
-		}
 		const API = JSONURL + yearFilter + publishFilter
 		fetch(API)
             .then(this.handleError)
             .then((data) => {
-				if (data.links.next){
+				const classNotesContainer = this._notesListElement
+				// if (data.links.next){
 					data.data.forEach(note=>{
 						notes.push(note)
 					})
-					this.getData(data.links.next.href,'',notes,true)
-				}
+				// 	this.getData(data.links.next.href,'',notes,true)
+				// }
 
-				if(!data.links.next){
+				// if(!data.links.next){
 					this.build(notes, year)
-				}
+				// }
 			})
             .catch(Error=> {
               console.error('There was an error fetching data from the API - Please try again later.')
@@ -51,24 +53,28 @@ class ClassNotesListElement extends HTMLElement {
 	}
 	// TO DO : construct
 	build(data, year){
-		// Build container
-		const classNotesContainer = document.createElement('div')
-			classNotesContainer.classList.add('ucb-class-notes-list-container')
+		const classNotesContainer = this._notesListElement
 		// Build Notes
 		data.forEach(note => {
 			const classNote = document.createElement('article')
 				classNote.classList.add('ucb-class-notes-list-note-item')
 			// Date
-			if(!year){
+			console.log('my year', year)
+
 				const classNoteYearContainer = document.createElement('div')
 					classNoteYearContainer.classList.add('class-note-year')
-				const classNoteYearLink = document.createElement('a')
+					const classNoteYearLink = document.createElement('a');
 					classNoteYearLink.href = '#';
 					classNoteYearLink.innerText = note.attributes.field_ucb_class_year;
-					classNoteYearLink.addEventListener('click', this.onYearSelect.bind(this));
+					
+					// Use an arrow function to pass the correct argument
+					classNoteYearLink.addEventListener('click', (event) => {
+						event.preventDefault(); // Prevent default anchor behavior
+						this.onYearSelect(note.attributes.field_ucb_class_year);
+					});
 				classNoteYearContainer.appendChild(classNoteYearLink)
 				classNote.appendChild(classNoteYearContainer)
-			}
+
 			// Class Note Text
 			const classNoteParagraph = document.createElement('p')
 				classNoteParagraph.innerHTML = this.escapeHTML(note.attributes.body.processed)
@@ -80,7 +86,7 @@ class ClassNotesListElement extends HTMLElement {
 				classNote.appendChild(classNotePosted)
 			classNotesContainer.appendChild(classNote)				
 		})
-			this.appendChild(classNotesContainer)
+			// this.appendChild(classNotesContainer)
 	}
 	toggleMessageDisplay(element, display, className, innerText) {
 		if(className)
@@ -166,13 +172,23 @@ class ClassNotesListElement extends HTMLElement {
 	// Event handler for the dropdown change
     onYearChange(event) {
         const year = event.target.value;
-        const JSONURL = this.getAttribute('base-uri');		
+        const JSONURL = this.getAttribute('base-uri');	
+		const notesListElement = this._notesListElement
+		while (notesListElement.firstChild) {
+			notesListElement.removeChild(notesListElement.firstChild);
+		}
 		this.getData(JSONURL, year);
     }
 
 	onYearSelect(year){
-		const JSONURL = this.getAttribute('base-uri')
-		this.getData(JSONURL, year)
+		const JSONURL = this.getAttribute('base-uri');
+		const notesListElement = this._notesListElement;
+	
+		while (notesListElement.firstChild) {
+			notesListElement.removeChild(notesListElement.firstChild);
+		}
+	
+		this.getData(JSONURL, year);
 	}
 
 	onSortChange(event){
