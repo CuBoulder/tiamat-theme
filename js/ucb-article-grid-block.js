2d483ff08e463a69a6dd39bc49bd2504667894f2
Pat <breepatbree@gmail.com>
2024-03-05 09:59:29 -0700

    Fixes bug with Article Grid duplicates

--- js/ucb-article-grid-block.js
+++ js/ucb-article-grid-block.js
@@ -165,11 +165,11 @@ class ArticleGridBlockElement extends HTMLElement {
         // Case for Too many articles
         if(finalArticles.length >= count || (finalArticles.length >= count && NEXTJSONURL)){
           finalArticles.length = count
-          this.renderDisplay(finalArticles, includeSummary)
       }
 
         // Have articles and want to proceed
-        if(finalArticles.length > 0 && !NEXTJSONURL){
+        if((finalArticles.length >= 0 && !NEXTJSONURL) || (finalArticles.length = count && NEXTJSONURL)){
+          finalArticles.length = count
           this.renderDisplay(finalArticles, includeSummary)
         }
     }
