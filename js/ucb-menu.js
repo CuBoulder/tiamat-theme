5ebd924cc534b0ec1ed88b88867dd0c10e8bf2ad
Jacob Korf <korfj921921@gmail.com>
2024-07-22 10:53:47 -0600

    More checks for mobile menu

--- js/ucb-menu.js
+++ js/ucb-menu.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   
-  const mainMenuCheck = document.getElementsByClassName("ucb-primary-menu-region-container")[0].getElementsByClassName("ucb-menu")[0];
+  const mainCheck = document.getElementsByClassName("ucb-primary-menu-region-container")[0];
+  if (mainCheck != null) {
+  const mainMenuCheck = mainCheck.getElementsByClassName("ucb-menu")[0];
   if (mainMenuCheck != null) {
     const mainMenu = new AccessibleMenu.DisclosureMenu({
     menuElement: mainMenuCheck,
@@ -9,31 +11,42 @@ document.addEventListener("DOMContentLoaded", () => {
     openClass: "open",
   });
 }
-  const secondaryCheck = document.getElementsByClassName("ucb-secondary-menu-region-container")[0].getElementsByClassName("ucb-menu")[0];
-  if (secondaryCheck != null) {
+  }
+const secondaryCheck = document.getElementsByClassName("ucb-secondary-menu-region-container")[0];
+if (secondaryCheck != null) {
+  const secondaryMenuCheck = secondaryCheck.getElementsByClassName("ucb-menu")[0];
+  if (secondaryMenuCheck != null) {
     const secondaryMenu = new AccessibleMenu.DisclosureMenu({
-      menuElement: secondaryCheck,
+      menuElement: secondaryMenuCheck,
       controllerElement: document.querySelector("#ucb-mobile-menu-toggle"),
       containerElement: document.getElementsByClassName("ucb-secondary-menu-region-container")[0],
       openClass: "open",
     });
   }
-  const footerCheck = document.getElementsByClassName("ucb-mobile-footer-menu")[0].getElementsByClassName("ucb-menu")[0];
+}
+  const footerCheck = document.getElementsByClassName("ucb-mobile-footer-menu")[0];
   if (footerCheck != null) {
+  const footerMenuCheck = footerCheck.getElementsByClassName("ucb-menu")[0];
+  if (footerMenuCheck != null) {
     const footerMenu = new AccessibleMenu.DisclosureMenu({
-      menuElement: footerCheck,
+      menuElement: footerMenuCheck,
       controllerElement: document.querySelector("#ucb-mobile-menu-toggle"),
       containerElement: document.getElementsByClassName("ucb-mobile-footer-menu")[0],
       openClass: "open",
     });
   }
-  const socialSecondaryCheck = document.getElementsByClassName("ucb-mobile-social-media-menu")[0].getElementsByClassName("social-media")[0];
+}
+  const socialSecondaryCheck = document.getElementsByClassName("ucb-mobile-social-media-menu")[0]
   if (socialSecondaryCheck != null) {
-    const socialSecondaryMenu = new AccessibleMenu.DisclosureMenu({
-      menuElement: socialSecondaryCheck,
-      controllerElement: document.querySelector("#ucb-mobile-menu-toggle"),
-      containerElement: document.getElementsByClassName("ucb-mobile-social-media-menu")[0],
-      openClass: "open",
-    });
+   const socialSecondaryMenuCheck = socialSecondaryCheck.getElementsByClassName("social-media")[0];
+   if (socialSecondaryMunuCheck != null) {
+     const socialSecondaryMenu = new AccessibleMenu.DisclosureMenu({
+       menuElement: socialSecondaryMenuCheck,
+       controllerElement: document.querySelector("#ucb-mobile-menu-toggle"),
+       containerElement: document.getElementsByClassName("ucb-mobile-social-media-menu")[0],
+       openClass: "open",
+     });
+   }
   }
-});
+}
+);
