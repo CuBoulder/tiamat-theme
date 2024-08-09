2f1f5fd4981ee770d70b05610f2d3ee9e996b2d6
Jacob Korf <korfj921921@gmail.com>
2024-08-09 12:47:26 -0600

    mega menu integration update

--- js/ucb-mega-menu.js
+++ js/ucb-mega-menu.js
@@ -1,16 +1,28 @@
 function megaMenuClicked(blockName) {
-    alert(blockName)
+    const allMegaMenus =  document.getElementsByClassName("ucb-mega-menu-wrapper");
     const blockID =  document.getElementsByClassName("block-" + blockName);
-    console.log(blockID);
+    let blockState = 0;
     for (let i = 0; i < blockID.length; i++) {
-        if(blockID[i].classList.contains("collapse"))
+        if(blockID[i].classList.contains("show")) {
+            blockState = 1;
+        }
+    }
+    for (let i = 0; i < allMegaMenus.length; i++) {
+        if(allMegaMenus[i].classList.contains("show"))
+        {
+            allMegaMenus[i].classList.remove("show")
+            allMegaMenus[i].classList.add("collapse")
+        }
+    }
+    for (let i = 0; i < blockID.length; i++) {
+        if(blockState == 0)
         {
             blockID[i].classList.add("show")
             blockID[i].classList.remove("collapse")
         }
         else {
-        blockID[i].classList.add("collapse")
-        blockID[i].classList.remove("show")
+            blockID[i].classList.add("collapse")
+            blockID[i].classList.remove("show")
         }
     }
 };
