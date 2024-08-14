6128e69605202dc72b1f39c8b0083c0c10dc0c06
Jacob Korf <korfj921921@gmail.com>
2024-08-14 08:55:15 -0600

    fix click outside, title, href

--- js/ucb-mega-menu.js
+++ js/ucb-mega-menu.js
@@ -1,3 +1,4 @@
+//Triggers when the mega menu link is clicked to show or hide the mega menu
 function megaMenuClicked(blockName) {
         const blockID =  document.getElementsByClassName("block-" + blockName);
         const allMegaMenus =  document.getElementsByClassName("ucb-mega-menu-wrapper");
@@ -27,16 +28,18 @@ function megaMenuClicked(blockName) {
         }
 };
 
+//Adds the click event listener to all mega menu outer links
 const megaMenuLinks = document.getElementsByClassName("ucb-mega-menu-outer-link");
 for (let i = 0; i < megaMenuLinks.length; i++) {
     if(megaMenuLinks[i].dataset.click.length > 0)
     megaMenuLinks[i].addEventListener("click", function(event){
+        event.preventDefault();
         megaMenuClicked(megaMenuLinks[i].dataset.click);
     });
 }
 
+//Adds the column value to the column wrapper
 const megaMenuColumns = document.getElementsByClassName("ucb-mega-menu-column-wrapper");
-
 for (let i = 0; i < megaMenuLinks.length; i++) {
     let megaMenuColumnCount = megaMenuColumns[i].children.length;
     if(megaMenuColumnCount < 6) {
@@ -47,6 +50,8 @@ for (let i = 0; i < megaMenuLinks.length; i++) {
     }
     megaMenuColumns[i].classList.add("ucb-mega-menu-column-" + megaMenuColumnCount);
 }
+
+//Adds a method for hiding the mega menu in mobile views when the mobile menu isn't open
 document.getElementById("ucb-mobile-menu-toggle").addEventListener("click", function(event){
     resizeFn();
 });
@@ -66,3 +71,17 @@ function resizeFn() {
     }
 }
  window.onresize = resizeFn;
+
+ //Add an even listener to check if the user clicked outside the mega menu, then close the mega menu
+ window.addEventListener('click', function(e){
+    const allMegaMenus =  document.getElementsByClassName("ucb-mega-menu-wrapper");
+    const allMainMenus =  document.getElementsByClassName("block-system-menu-blockmain");
+    for (let i = 0; i < allMegaMenus.length; i++) {
+        for (let j = 0; i < allMainMenus.length; i++) {
+            if (!allMegaMenus[i].contains(e.target) && !allMainMenus[j].contains(e.target) && allMegaMenus[i].classList.contains("show")){
+                allMegaMenus[i].classList.remove("show")
+                allMegaMenus[i].classList.add("collapse")
+            }
+        }
+    }
+})
