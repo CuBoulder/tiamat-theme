301c660da0ef1afd903d7584a511e952d2cad7e6
Jacob Korf <korfj921921@gmail.com>
2024-08-16 14:46:18 -0600

    fix menu styles, collapse

--- js/ucb-mega-menu.js
+++ js/ucb-mega-menu.js
@@ -1,51 +1,9 @@
-//Triggers when the mega menu link is clicked to show or hide the mega menu
-function megaMenuClicked(blockName) {
-  const blockID =  document.getElementsByClassName("block-" + blockName);
-  let allMegaMenus =  document.getElementsByClassName("ucb-mega-menu-wrapper");
-  let blockState = 0;
-  for (let i = 0; i < blockID.length; i++) {
-    if(blockID[i].classList.contains("show")) {
-      blockState = 1;
-    }
-  }
-
-  for (let i = 0; i < allMegaMenus.length; i++) {
-    if(allMegaMenus[i].classList.contains("show"))
-    {
-      allMegaMenus[i].classList.remove("show");
-      allMegaMenus[i].classList.add("collapse");
-    }
-  }
-
-  for (let i = 0; i < blockID.length; i++) {
-    if(blockState == 0)
-    {
-      blockID[i].classList.add("show");
-      blockID[i].classList.remove("collapse");
-    }
-    else {
-      blockID[i].classList.add("collapse");
-      blockID[i].classList.remove("show");
-    }
-  }
-};
-
-//Adds the click event listener to all mega menu outer links
-let megaMenuLinks = document.getElementsByClassName("ucb-mega-menu-outer-link");
-for (let i = 0; i < megaMenuLinks.length; i++) {
-  if(megaMenuLinks[i].dataset.click.length > 0) {
-    megaMenuLinks[i].addEventListener("click", function(event){
-      event.preventDefault();
-      megaMenuClicked(megaMenuLinks[i].dataset.click);
-    });
-  }
-}
-
 //Adds the column value to the column wrapper
+let megaMenuLinks = document.getElementsByClassName("ucb-mega-menu-outer-link");
 let megaMenuColumns = document.getElementsByClassName("ucb-mega-menu-column-wrapper");
 for (let i = 0; i < megaMenuLinks.length; i++) {
   let megaMenuColumnCount = megaMenuColumns[i].children.length;
-  if(megaMenuColumnCount < 6) {
+  if(megaMenuColumnCount < 3) {
     megaMenuColumnCount = 3;
   }
   if(megaMenuColumnCount > 6) {
@@ -54,16 +12,15 @@ for (let i = 0; i < megaMenuLinks.length; i++) {
   megaMenuColumns[i].classList.add("ucb-mega-menu-column-" + megaMenuColumnCount);
 }
 
- //Add an even listener to check if the user clicked outside the mega menu, then close the mega menu
+ //Add an event listener to check if the user clicked outside the mega menu, then close the mega menu
  window.addEventListener('click', function(e){
   const allMegaMenus =  document.getElementsByClassName("ucb-mega-menu-wrapper");
   const allMainMenus =  document.getElementsByClassName("block-system-menu-blockmain");
   for (let i = 0; i < allMegaMenus.length; i++) {
     for (let j = 0; j < allMainMenus.length; j++) {
       if (!allMegaMenus[i].contains(e.target) && !allMainMenus[j].contains(e.target) && allMegaMenus[i].classList.contains("show")){
-        console.log(allMegaMenus[i].classList);
         allMegaMenus[i].classList.remove("show");
-        allMegaMenus[i].classList.add("collapse");
+        allMegaMenus[i].ariaExpanded = "false";
       }
     }
   }
