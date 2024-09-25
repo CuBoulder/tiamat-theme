7ffe8b934345bddbcee60b2f02ec92f81bf0e9ee
Jacob Korf <korfj921921@gmail.com>
2024-09-25 09:34:28 -0600

    Hide Hamburger in all cases with no menus

--- js/ucb-menu.js
+++ js/ucb-menu.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const checkMenuExistence = document.getElementsByClassName("ucb-main-nav-container")[0];
+  if (checkMenuExistence != null) {
+    const quantityMenus = checkMenuExistence.getElementsByClassName("menu-item");
+    if(quantityMenus.length < 1) {
+      document.getElementById("ucb-mobile-menu-toggle").style.display = "none";
+    }
+  }
+
   const sectionCheck = document.getElementsByClassName("ucb-main-nav-section")[0];
   if (sectionCheck != null) {
     const menuCheck = sectionCheck.getElementsByClassName("ucb-main-nav-container")[0];
