e1af009c4c3efa637b7095f67e48879230bf9728
Jacob Korf <korfj921921@gmail.com>
2024-10-31 13:45:50 -0500

    Re add mobile menu

---
+++ js/ucb-menu.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+//Build Mobile Menu
+let quantityMenusLength = 0;
+const checkMenuExistence = document.getElementsByClassName("ucb-main-nav-container")[0];
+if (checkMenuExistence != null) {
+  const quantityMenus = checkMenuExistence.getElementsByClassName("menu-item");
+  quantityMenusLength = quantityMenus.length;
+  if(quantityMenus.length < 1 && document.getElementById("ucb-mobile-menu-toggle") != null) {
+    document.getElementById("ucb-mobile-menu-toggle").style.display = "none";
+  }
+}
+if(quantityMenusLength > 0) {
+  const sectionCheck = document.getElementsByClassName("ucb-main-nav-section")[0];
+  if (sectionCheck != null) {
+    const menuCheck = sectionCheck.getElementsByClassName("ucb-main-nav-container")[0];
+    if (menuCheck != null) {
+      const mobileMenu = new AccessibleMenu.DisclosureMenu({
+        menuElement: menuCheck,
+        controllerElement: document.querySelector("#ucb-mobile-menu-toggle"),
+        containerElement: sectionCheck,
+        openClass: "open",
+      });
+    }
+  }
+}
+});
